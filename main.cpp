1c099f2f4959a6a0b4425e9638e034730ad0cfa6
iyedbenaicha <iyed.benaicha@supcom.tn>
2026-06-09 12:23:07 +0100

    create a hello world project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by iyedbenaicha on 6/9/26.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by iyedbenaicha on 6/9/26.
+//
+
+#ifndef CODING_BETOUNSI_MAIN_H
+#define CODING_BETOUNSI_MAIN_H
+
+#endif //CODING_BETOUNSI_MAIN_H
