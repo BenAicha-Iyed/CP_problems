201dbac2dd04c555d8f76eabdb8c82f16b41ea90
iyedbenaicha <iyed.benaicha@supcom.tn>
2026-06-23 12:22:09 +0100

    add Kefa and Park problem

--- contests/Codeforces Round 321 (Div 2)/Kefa and Park.h
+++ contests/Codeforces Round 321 (Div 2)/Kefa and Park.h
@@ -1,6 +1,12 @@
 //
-// Created by iyedbenaicha on 6/9/26.
+// Created by iyedbenaicha on 6/23/26.
+// problem link: https://codeforces.com/contest/580/problem/C
 //
+
+#ifndef CODING_BETOUNSI_KEFA_AND_PARK_H
+#define CODING_BETOUNSI_KEFA_AND_PARK_H
+
+#endif //CODING_BETOUNSI_KEFA_AND_PARK_H
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,7 +18,7 @@ typedef vector<vi> vvi;
 
 class Solution {
 private:
-    int n, m;
+    int n = 0, m = 0;
     vvi tree;
     vi consecutive_cats, cats, parent;
     vb visited;
@@ -58,19 +64,3 @@ public:
         cout << ans << endl;
     }
 };
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    // freopen("errors.txt", "w", stderr);
-    Solution s;
-    int t=1;
-    // cin>>t;
-    while (t--) {
-        s.solve();
-    }
-    return 0;
-}
