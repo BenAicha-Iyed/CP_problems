0c42fa407b425c0f0cb79d9738ea49d942207367
iyedbenaicha <iyed.benaicha@supcom.tn>
2026-07-22 14:58:04 +0100

    Add RoundTrip.h

--- CSES/RoundTrip.h
+++ CSES/RoundTrip.h
@@ -1,6 +1,12 @@
 //
-// Created by iyedbenaicha on 6/9/26.
+// Created by iyedbenaicha on 7/22/26.
+// problem link: https://cses.fi/problemset/result/18044245/
 //
+
+#ifndef CODING_BETOUNSI_ROUNDTRIP_H
+#define CODING_BETOUNSI_ROUNDTRIP_H
+
+#endif //CODING_BETOUNSI_ROUNDTRIP_H
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -66,19 +72,3 @@ public:
     }
 };
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("errors.txt", "w", stderr);
-
-    int t=1;
-    // cin>>t;
-    while (t--) {
-        Solution s;
-        s.solve();
-    }
-    return 0;
-}
